b4c3d9fc3af9d8d719b71c9a6ef4d672ff0021b4
GGP77 <gomesgabriel77ggp@gmail.com>
2025-05-01 16:27:05 +0000

    criei medias de notas

---
+++ ManipulacaoeConversao.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    //int a = 10;
+    //int b = 3;
+    //float quociente = (float) a / b; //"a" é explicidamente convertido para float - float entre parenteses chama-se casting
+
+    //printf("Quociente: %.2f\n", quociente);
+
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("*** Programa de Cálculo de Média *** \n");
+
+    printf("Digite a Nota 1: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite a Nota 2: \n");
+    scanf("%f", &nota2);
+
+    printf("Digite a Nota 3: \n");
+    scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A Média das notas é: %.2f", media);
+
+
+
+    return 0;
+
+}
