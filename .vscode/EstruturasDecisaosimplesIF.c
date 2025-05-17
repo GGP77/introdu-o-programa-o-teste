ac33e4f793c9601d80d95091288b3a3b3a1af9fc
GGP77 <gomesgabriel77ggp@gmail.com>
2025-05-17 19:12:04 +0000

    criei if simples

---
+++ .vscode/EstruturasDecisaosimplesIF.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+   
+  /*int numero1, numero2;
+
+  numero1 = 10;
+  numero2 = 5;
+  //if só executa se for verdadeiro
+  if (numero1 > numero2){ 
+     printf("Numero 1 é maior que Numero 2");
+}*/
+  
+/*int idade = 20;
+
+if (idade >= 18){
+    printf("Você é maior de idade\n");//fora das chaves só executa a primeira linha
+}*/
+
+int numero = 4, resultado;
+
+resultado = numero % 2;
+printf("O resultado da variável é: %d\n", resultado);
+
+if (resultado == 0){
+    printf("O número é par\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ .vscode/cartasteste.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+    int main(){
+    printf("Cartas Super Trunfo! \n");
+
+    int pontosturisticos, populacao;
+    char cidade[50], estado[30], codigo[10];
+    float area, pib, superpoder;
+    float densidadepopulacional, pibpercapta;
+    
+printf("Informe o Estado: \n");
+scanf("%s", &estado);
+
+printf("Informe o Código da Carta: \n");
+scanf("%s", &codigo);
+
+printf("Informe o Nome da Cidade: \n");
+scanf("%s", &cidade);
+
+printf("Informe a População da Cidade: \n");
+scanf("%d", &populacao);
+
+printf("Informe a Área da Cidade: \n");
+scanf("%f", &area);
+
+printf("Informe o PIB da Cidade: \n");
+scanf("%f", &pib);
+
+printf("Informe o Número de Pontos Turísticos da Cidade: \n");
+scanf("%d", &pontosturisticos);
+
+densidadepopulacional = (float) populacao / area;
+pibpercapta = (float) pib / populacao;
+superpoder = populacao + area + pib + pontosturisticos + pibpercapta + (1 / densidadepopulacional);
+
+printf("Estado: %s\n", estado);
+printf("Código da Carta: %s\n", codigo);
+printf("Nome da Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %.3f km²\n", area);
+printf("PIB: %.3f bilhões de reais\n", pib);
+printf("Pontos Turísticos: %d\n", pontosturisticos);
+printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional);
+printf("PIB per Capta: %.2f reais\n", pibpercapta);
+printf("Super Poder: %f\n", superpoder);
+
+    int Pontosturisticos, Populacao;
+    char Cidade[50], Estado[30], Codigo[10];
+    float Area, Pib, Densidadepopulacional, Pibpercapta, SuperPoder;
+    
+printf("Informe o Estado: \n");
+scanf("%s", &Estado);
+
+printf("Informe o Código da Carta: \n");
+scanf("%s", &Codigo);
+
+printf("Informe o Nome da Cidade: \n");
+scanf("%s", &Cidade);
+
+printf("Informe a População da Cidade: \n");
+scanf("%d", &Populacao);
+
+printf("Informe a Área da Cidade: \n");
+scanf("%f", &Area);
+
+printf("Informe o PIB da Cidade: \n");
+scanf("%f", &Pib);
+
+printf("Informe o Número de Pontos Turísticos da Cidade: \n");
+scanf("%d", &Pontosturisticos);
+
+Densidadepopulacional = (float) Populacao / Area; 
+Pibpercapta = (float) Pib / Populacao;
+SuperPoder = Populacao + Area + Pib + Pontosturisticos + Pibpercapta + (1 / Densidadepopulacional);
+
+printf("Estado: %s\n", Estado);
+printf("Código da Carta: %s\n", Codigo);
+printf("Nome da Cidade: %s\n", Cidade);
+printf("População: %d\n", Populacao);
+printf("Área: %.2f km²\n", Area);
+printf("PIB: %.3f bilhões de reais\n", Pib);
+printf("Pontos Turísticos: %d\n", Pontosturisticos);
+printf("Densidade Populacional: %.2f hab/km²\n", Densidadepopulacional);
+printf("PIB per Capta: %.2f reais\n", Pibpercapta);
+printf("Super Poder: %f\n", SuperPoder);
+
+int resultadoPopulacao, resultadoArea, resultadoPib, resultadoPontoTuristico, resultadoDensidade, resultadoPibPercapta, 
+     resultadoSuper;
+resultadoPopulacao = Populacao > populacao;
+resultadoArea = Area > area;
+resultadoPib = Pib > pib;
+resultadoPontoTuristico = Pontosturisticos > pontosturisticos;
+resultadoDensidade = Densidadepopulacional < densidadepopulacional;
+resultadoPibPercapta = Pibpercapta > pibpercapta;
+resultadoSuper = SuperPoder > superpoder;
+
+printf("Comparação das Cartas: \n");
+
+printf("População: %d\n", resultadoPopulacao);
+printf("Área: %d\n", resultadoArea);
+printf("PIB: %d\n", resultadoPib);
+printf("Pontos Turísticos: %d\n", resultadoPontoTuristico);
+printf("Densidade Populacional: %d\n", resultadoDensidade);
+printf("PIB per Capta: %d\n", resultadoPibPercapta);
+printf("Super Poder: %d\n", resultadoSuper);
+
+return 0;
+}
