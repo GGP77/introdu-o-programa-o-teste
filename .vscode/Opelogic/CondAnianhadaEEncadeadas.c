6b982f6b1c59a99ec22d788932fa826a21d16e7b
GGP77 <gomesgabriel77ggp@gmail.com>
2025-05-23 00:17:17 +0000

    criei aninhadades e encadeadas

---
+++ .vscode/Opelogic/CondAnianhadaEEncadeadas.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int condicao1, condicao2;
+
+    //estrutura Aninhada
+    if(condicao1){
+      if(condicao2){
+        //Código a ser executado se condicao1 e condicao2 forem verdadeiras
+      }
+    }
+
+    //Estrutura Encadeada
+    if(condicao1){
+        //Código a ser executado se condicao1 for verdadeira
+    } else if(condicao2){
+        //Código a ser executado se condicao1 for falso e condicao2 for verdadeira
+    } else {
+        //Código a ser executado se todas as condições anteriores forem falsas
+    }
+
+
+
+
+}
